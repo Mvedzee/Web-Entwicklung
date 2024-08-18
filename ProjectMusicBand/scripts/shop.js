623eb5efa467041f1ab83870182b774c0c99960c
Mvedzee <149262809+Mvedzee@users.noreply.github.com>
2024-08-18 16:36:31 +0200

    Add files via upload
    Musik Band website

---
+++ ProjectMusicBand/scripts/shop.js
@@ -0,0 +1,27 @@
+// Function to add item to cart
+function addToCart(name, price, picture) {
+    console.log(`Adding ${name} to cart`);
+    const cart = JSON.parse(sessionStorage.getItem('cart')) || [];
+    cart.push({ name, price, picture });
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+    alert(name + ' wurde zum Warenkorb hinzugefügt!');
+    displayCartItems(); // To update the cart display immediately
+    updateTotalPrice();
+}
+        //alle elemente im Dokument
+document.querySelectorAll('.fas.fa-heart').forEach(function(element) {
+    // Add click event listener to each element
+    element.addEventListener('click', function() {
+            //ergrbnisse aud HTML-elemente zu reagrieren
+        // Toggle the active class on the clicked element
+        element.classList.toggle('active');
+    });
+});
+
+// Function to toggle like button
+function toggleLike(element) {
+    console.log('Toggling like');
+    element.classList.toggle('liked');
+}
+
+console.log('shop.js loaded');
